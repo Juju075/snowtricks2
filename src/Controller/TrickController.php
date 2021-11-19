7eafa3a4b91753ab55c8b24d2cb9a30f3a9cd1a7
elgato <amzfba.1bestbuy@gmail.com>
2021-11-19 16:10:15 +0100

    Webpacencore and Yarn installed not configured yet!

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Form\TrickType;
+use App\Repository\TrickRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class TrickController extends AbstractController
 {
     /**
-     * @Route("/trick", name="trick")
+     * @Route("/", name="app_home", methods={"GET"})
      */
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
-        ]);
+        $tricks = $trickRepository->findBy([], ['createdAt'=>'DESC']);
+        return $this->render('tricks/index.html.twig', compact('tricks'));
     }
 }
