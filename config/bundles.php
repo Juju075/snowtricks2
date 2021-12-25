0f0602304d7d8d1667f98a9bc0d1856b2e991407
elgato <amzfba.1bestbuy@gmail.com>
2021-12-25 13:52:46 +0100

    installed orm fixture

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
