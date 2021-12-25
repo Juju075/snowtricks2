c87ab76a0aa200895fd5513f2829a1c711f0d744
elgato <amzfba.1bestbuy@gmail.com>
2021-12-25 11:14:32 +0100

    in progress

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -5,11 +5,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-use Symfony\Component\Validator\Constraints as Assert;
-use App\Repository\TrickRepository;
 use App\Entity\Traits\Timestampable;
-
-use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
