b761e23ac9cf3da1c49451fd5499dc91e91f30c7
elgato <amzfba.1bestbuy@gmail.com>
2021-12-27 22:22:59 +0100

    DataFixtures/TricksFixtures:

--- src/DataFixtures/TricksFixtures.php
+++ src/DataFixtures/TricksFixtures.php
@@ -17,17 +17,11 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class TricksFixtures extends Fixture implements DependentFixtureInterface
 {
+    // Relations de Trick::class (target fk) - user comments photos videos category
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
-        // Relations de Trick::class (target fk) - user comments photos videos category
 
-        //Mettre en boucle pour generer des imgs di
-        //permet d'enlever le path et garder juste le name de la photo 47:08
-        $photo = $faker->image('public/uploads/'); // path de destination , taille et random. vu pas d'extension!!!
-        $photoName = str_replace('public/uploads\\', '', $photo);
-
-        //Pour la creation d'un trick
         for ($nbTricks=1; $nbTricks <= 40 ; $nbTricks++) { 
             //pour un utilisateur en reference de facon aleatoire reuse user_id
             $user = $this->getReference('user_'. $faker->numberBetween(1 ,10));
@@ -39,17 +33,34 @@ class TricksFixtures extends Fixture implements DependentFixtureInterface
             $trick->setNom($faker->realText(25));
             $trick->setDescription($faker->realText(400));
 
-            //Inputs de Fichiers.
-
             //upload photos 3 img par trick
             for ($image=0; $image <= 2 ; $image++) { 
                 //genere une nouvelle image a chaque boucle
-                $photo = $faker->image('public/uploads');
-                $photoName = basename($photo);
+                //$photo = $faker->image('public/uploads/'); //image() path de destination , taille et random. vu pas d'extension!!!
+                
+                //$photoName = str_replace('public/uploads\\', '', $photo);
+                //$photoName = basename($photo);
+                $photoName = array(
+                    '1'=>'3a2efcb1dd5be46ad242348d1155470d.jpg',
+                    '2'=>'0508f7e1f32150aaf16b4545a7438fcc.jpg',
+                    '3'=>'b522c2816511c322fd450185b8fcc7ce.jpg',
+                    '4'=>'4ccc18a0d10cd25d778ec2eba1ec9a82.jpg',
+                    '5'=>'a252a944f109d4a87de24fadbf2b3173.jpg',
+                    '6'=>'a7fe1d19ba3580ad8f6acc3272659e0d.jpg',
+                    '7'=>'72905b91613379722cecaac74f67e982.jpg',
+                    '8'=>'92af130caab728520c70853bc35d84aa.jpg',
+                    '9'=>'449360a6bf7581c30247140e30e255ab.jpg',
+                    '10'=>'a81eb2a5105a43366421cc7d3e3cf870.jpg',
+                    '11'=>'8b58d78fc1d28fc5b446b3dd78379ff0.jpg',
+                    '12'=>'dcbb0da03b5840d6574abf507b58550a.jpg',
+                    '13'=>'f20ad0211ab29ad8d29b899573604c22.jpg',
+                    '14'=>'99b47532cf17093c95e84c6e3ff7d2c3.jpg',
+                    '15'=>'4ae9be30a99889f9d4b7769f100e6a63.jpg'
+                );
+                shuffle($photoName); 
 
                 $photoTrick = new Photo();
-                //$photoTrick->setName(array_rand($listOfImages));
-                $photoTrick->setName($photoName);
+                $photoTrick->setName($photoName[0]);
                 $trick->addPhoto($photoTrick);
             }
             
@@ -74,7 +85,6 @@ class TricksFixtures extends Fixture implements DependentFixtureInterface
 
         }
         $manager->flush();
-        $this->addRandomComments();
     }
 
     public function addRandomComments(User $user, Trick $tricks, ObjectManager $manager)
